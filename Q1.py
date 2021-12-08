e4b7f754400e3b82a7e4a93e7f9b482b06e9db31
riyanshi23 <91719142+riyanshi23@users.noreply.github.com>
2021-12-08 16:24:37 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -6,17 +6,6 @@ a = 3.4536
 print ("The value of number till 2 decimal place(using %) is : ",end="")
 print ('%.2f'%a)
  
-# using format() to print value till 2 decimal places
-print ("The value of number till 2 decimal place(using format()) is : ",end="")
-print ("{0:.2f}".format(a))
- 
-# using round() to print value till 2 decimal places
-print ("The value of number till 2 decimal place(using round()) is : ",end="")
-print (round(a,2))
-
-
 # Output : 
 
 # The value of number till 2 decimal place(using %) is : 3.45
-# The value of number till 2 decimal place(using format()) is : 3.45
-# The value of number till 2 decimal place(using round()) is : 3.45
