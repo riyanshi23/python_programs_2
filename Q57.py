50413feb3a983c1fee5eb6fa8238affcdd25fc83
riyanshi23 <91719142+riyanshi23@users.noreply.github.com>
2021-12-08 22:47:24 +0530

    Update Q57.py

--- Q57.py
+++ Q57.py
@@ -1,17 +1,7 @@
-
-n = int(input("Enter a number: "))
-
-def armstrong(num):
-  sum = 0
-  temp = num
-  while temp > 0:
-    digit = temp % 10
-    sum += digit ** 3
-    temp //= 10
-  return sum
-
-summ=armstrong(n)
-if n == summ:
-   print(n,"is an Armstrong number")
-else:
-   print(n,"is not an Armstrong number")
+def isprime(num):
+    for n in range(2,int(num**1/2)+1):
+        if num%n==0:
+            return False
+    return True
+print(isprime(7))
+print(isprime(8))
