4e5019c62c270060d4858a27575ddf6669f8c7c1
riyanshi23 <91719142+riyanshi23@users.noreply.github.com>
2021-12-08 18:03:19 +0530

    Create Q6.py

---
+++ Q6.py
@@ -0,0 +1,9 @@
+# Q6 - write a python program to calculate the length of string 
+str = input("Enter a string: ")
+counter = 0
+for s in str:
+      counter = counter+1
+print("Length of the input string is:", counter)
+#output 
+Enter a string: Beginnersbook
+Length of the input string is: 13
