168c17ddcc62640118a91f7ef47b02caa2f23dcc
riyanshi23 <91719142+riyanshi23@users.noreply.github.com>
2021-12-08 18:05:48 +0530

    Create Q8.py

---
+++ Q8.py
@@ -0,0 +1,14 @@
+#Q8. given two integer number return there sum if sum is greater than 100 then return there product 
+
+n1 = int(input("enter a number"))
+n2 = int(input("enter another number"))
+product=n1*n2
+If product>100:
+      print(n1+n2)
+else:
+      print(product)
+
+#output 
+enter a number 6
+enter another number 9
+54
