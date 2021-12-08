90d85949a55cdbfa756fc4afac56605a40b1482d
riyanshi23 <91719142+riyanshi23@users.noreply.github.com>
2021-12-08 22:43:53 +0530

    Create Q62.py

---
+++ Q62.py
@@ -0,0 +1,9 @@
+list1 = [11, 5, 17, 18, 23]
+total=0
+for i in range(0, len(list1)):
+    total = total + list1[i]
+ 
+print("Sum of all elements in given list: ", total)
+Sum of all elements in given list:  74
+  
+ 
