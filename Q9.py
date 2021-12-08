fdbaddb28855253045a365c0a6a9ab6f3cf5ff9a
riyanshi23 <91719142+riyanshi23@users.noreply.github.com>
2021-12-08 20:55:48 +0530

    Update Q9.py

--- Q9.py
+++ Q9.py
@@ -1,7 +1,7 @@
 first = int(input("enter first number : "))
-second = int(input("enter first number : "))
-third = int(input("enter first number : "))
-fourth = int(input("enter first number : "))
+second = int(input("enter second number : "))
+third = int(input("enter third number : "))
+fourth = int(input("enter fourth  number : "))
 numberList = [first, second, third, fourth]
 sum = numberList[0] + numberList[1] + numberList[2] + numberList[3]
 print(sum)
